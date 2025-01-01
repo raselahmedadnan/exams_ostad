cde3685bc7f93c5485353e81f5d9f69a3973d5f1
Rasel Ahamed Adnan <raselahmedadnan@gmail.com>
2025-01-01 20:14:14 +0300

    Module 5 update

---
+++ bin/Module5 Exams/live_exam_M5.dart
@@ -0,0 +1,14 @@
+void triangle(){
+  //input values;
+  double base = 12;
+  double height = 4;
+
+
+  double area = 0.5 * base * height; //calculate area
+  
+  print("The area of the triangle is $area");
+}
+
+void main(){
+  triangle();
+}
